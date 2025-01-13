d05822f4c77a33515ebc41f915d44cea8794124a
Eduarda Matias <evcm2003@gmail.com>
2025-01-13 16:37:51 -0300

    Criação do projeto
    - Início do projeto com configurações iniciais.
    - Remoção de arquivos e configurações padrão desnecessárias gerados automaticamente pela criação do projeto.
    - Preparação do ambiente para desenvolvimento.

---
+++ Controller/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace controle_financeiro_api.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloWorld : ControllerBase
+    { 
+        [HttpGet()]
+        public IActionResult Get()
+        {
+            return Ok("Hello World!!!! 🤗");
+        }
+    }
+}
