e338d8f4f9a5c7012e6d56d6f28a6fb3e500bfa3
Eduarda Matias <evcm2003@gmail.com>
2025-01-16 17:59:31 -0300

    Adicionando validação na requisição de criar/editar usuários

--- Model/DTO/Request/UsuarioAdicionarSaldoRequest.cs
+++ Model/DTO/Request/UsuarioAdicionarSaldoRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace controle_financeiro_api.Model.DTO.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace controle_financeiro_api.Model.DTO.Request
 {
     public class UsuarioAdicionarSaldoRequest
     {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public int Saldo { get; set; }
     }
 }
