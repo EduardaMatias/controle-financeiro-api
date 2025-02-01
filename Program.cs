6a5e6b05f709ed52883788d1a47f4bb84cc3897a
Eduarda Matias <evcm2003@gmail.com>
2025-02-01 00:56:34 -0300

    Adicionando swagger em prod

--- Program.cs
+++ Program.cs
@@ -110,15 +110,10 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
-app.UseAuthentication(); 
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
