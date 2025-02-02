2d3b5289d448f50dfefd43dbc08f42b10d3de68b
Eduarda Matias <evcm2003@gmail.com>
2025-02-02 15:13:16 -0300

    Atualização dos Scripts
    - Scripts utilizados para a criação das tabelas no neon (PostgreSQL)

--- Scripts.sql
+++ Scripts.sql
@@ -1,54 +1,48 @@
--- Criação do banco ControleFinanceiro
-CREATE DATABASE ControleFinanceiro;
-
-USE ControleFinanceiro;
-
--- Criação da tabela Usuario
 CREATE TABLE Usuario (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
-    Nome NVARCHAR(100) NOT NULL,
-    Email NVARCHAR(255) NOT NULL UNIQUE,
-    Senha NVARCHAR(100) NOT NULL,
-    Saldo DECIMAL(18, 2) NULL DEFAULT 0
+    "id" SERIAL PRIMARY KEY,
+    "Nome" VARCHAR(100) NOT NULL,
+    "Email" VARCHAR(255) NOT NULL UNIQUE,
+    "Senha" VARCHAR(100) NOT NULL,
+    "Saldo" DECIMAL(18, 2) DEFAULT 0
 );
 
--- Criação da tabela Planejamento
-CREATE TABLE Planejamento (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
-    Usuario_Id INT NOT NULL FOREIGN KEY REFERENCES Usuario(Id),
-    Valor DECIMAL(18, 2) NOT NULL,
-    Mes NVARCHAR(20) NOT NULL
+CREATE TABLE "Planejamento" (
+    "id" SERIAL PRIMARY KEY,
+    "Usuario_Id" INT NOT NULL REFERENCES "usuario"("id"),
+    "Valor" DECIMAL(18, 2) NOT NULL,
+    "Mes" VARCHAR(20) NOT NULL
 );
 
--- Criação da tabela Receita
-CREATE TABLE Receita (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
-    Usuario_Id INT NOT NULL FOREIGN KEY REFERENCES Usuario(Id),
-    Valor DECIMAL(18, 2) NOT NULL,
-    Moeda NVARCHAR(10) NOT NULL,
-    Data DATE NOT NULL,
-    Categoria NVARCHAR(50) NOT NULL
+CREATE TABLE "Receita" (
+    "id" SERIAL PRIMARY KEY,
+    "Usuario_Id" INT NOT NULL REFERENCES "usuario"("id"),
+    "Valor" DECIMAL(18, 2) NOT NULL,
+    "Moeda" VARCHAR(10) NOT NULL,
+    "Data" DATE NOT NULL,
+    "Categoria" VARCHAR(50) NOT NULL
 );
 
--- Criação da tabela Despesa
-CREATE TABLE Despesa (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
-    Usuario_Id INT NOT NULL FOREIGN KEY REFERENCES Usuario(Id),
-    Valor DECIMAL(18, 2) NOT NULL,
-    Moeda NVARCHAR(10) NOT NULL,
-    Data DATE NOT NULL,
-    Categoria NVARCHAR(50) NOT NULL
+CREATE TABLE "Despesa" (
+    "id" SERIAL PRIMARY KEY,
+    "Usuario_Id" INT NOT NULL REFERENCES "usuario"("id"),
+    "Valor" DECIMAL(18, 2) NOT NULL,
+    "Moeda" VARCHAR(10) NOT NULL,
+    "Data" DATE NOT NULL,
+    "Categoria" VARCHAR(50) NOT NULL
 );
 
--- Criação da tabela Historico
-CREATE TABLE Historico (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
-    Usuario_Id INT NOT NULL FOREIGN KEY REFERENCES Usuario(Id),
-    Tipo NVARCHAR(50) NOT NULL,
-    Valor DECIMAL(18, 2) NOT NULL,
-    Data DATE NOT NULL
+CREATE TABLE "Historico" (
+    "id" SERIAL PRIMARY KEY,
+    "Usuario_Id" INT NOT NULL REFERENCES "usuario"("id"),
+    "Tipo" VARCHAR(50) NOT NULL,
+    "Valor" DECIMAL(18, 2) NOT NULL,
+    "Data" DATE NOT NULL,
+    "Categoria" VARCHAR(50) NOT NULL
 );
 
--- Alteração na tabela Historico
-ALTER TABLE [ControleFinanceiro].[dbo].[Historico]
-ADD [Categoria] VARCHAR(50) NOT NULL;
+ALTER TABLE usuario RENAME TO "Usuario";
+ALTER TABLE "Usuario" RENAME COLUMN id TO "Id";
+ALTER TABLE "Historico" RENAME COLUMN id TO "Id";
+ALTER TABLE "Receita" RENAME COLUMN id TO "Id";
+ALTER TABLE "Despesa" RENAME COLUMN id TO "Id";
+ALTER TABLE "Planejamento" RENAME COLUMN id TO "Id";
